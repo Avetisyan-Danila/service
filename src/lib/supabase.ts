897a16dfa7da1ce93498f0bbffb5b189898c82da
davetisyan <davetisyan@eqvantalab.ru>
2026-01-12 18:59:18 +0400

    Захардкодил env переменные

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-	import.meta.env.VITE_SUPABASE_URL,
-	import.meta.env.VITE_SUPABASE_ANON_KEY
+	'https://uvrztvtagoosuzacdwma.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2cnp0dnRhZ29vc3V6YWNkd21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNDUzMzEsImV4cCI6MjA3MzYyMTMzMX0.A1cRIcjWd1-YhWO1P3-9MJNUB5bmpCIA2KLXxRmSkAM'
 )
